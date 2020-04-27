08bcc1e4e96220371e39da51b6b30522643d65ae
unknown <t.johnson57@students.clark.edugit config --global user.email t.johnson57@students.clark.edu>
2020-04-27 12:32:31 -0700

    completed class lab

---
+++ lab2-1.py
@@ -0,0 +1,43 @@
+"""
+CTEC 121
+Tyler Johnson   
+module 2 lab 1
+<assignment/lab description
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    """
+    A = 2
+    print("\n", A, "\n")
+    a = 22
+    print(a)
+    print()
+    spam = 222
+    print(spam)
+    print()
+    #expressions
+    #print(2)
+    print(3-1)
+    print((3-1)/2)
+    print(5%3)
+    print(2+((3-1)/2))
+
+    print()
+    print("The answer is:", 3+4 )
+    print()
+    """
+
+#excercize 
+
+    
+    classcode = "CTEC 121"
+    classcount = 18
+    classname = "introduction to programming and problem solving"  
+    print("Welcome to ", classcode, ". The student count is ", classcount," and the name of the class is ",classname, ".", sep="")
+main()    
